3fe0aa6d9ccb51c0d56f7c0fbe611d795e1b0250
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-24 18:40:45 +0000

    corrections to the python script

--- factors.py
+++ factors.py
@@ -1,48 +1,50 @@
 import sys
 import math
 
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def pollards_rho(n):
-    if n % 2 == 0:
-        return 2
-
-    x = 2
-    y = 2
-    d = 1
-
-    f = lambda x: (x**2 + 1) % n
-
-    while d == 1:
-        x = f(x)
-        y = f(f(y))
-        d = gcd(abs(x - y), n)
-
-    return d
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, math.isqrt(num) + 1):
+        if num % i == 0:
+            return False
+    return True
 
 def factorize_number(number):
     if number <= 1:
-        return [number]
+        print(f"{number}={number}")
+        return
 
-    factors = []
+    print(f"{number}=", end='')
+
     while number > 1:
-        factor = pollards_rho(number)
-        factors.append(factor)
-        number //= factor
+        factor_found = False
 
-    return factors
+        # Check if the number itself is prime
+        if is_prime(number):
+            print(f"{number}")
+            break
+
+        # Find the smallest prime factor using Pollard's rho algorithm
+        for factor in range(2, number + 1):
+            if number % factor == 0 and is_prime(factor):
+                print(f"{factor}", end='')
+                number //= factor
+                factor_found = True
+                if number > 1:
+                    print("*", end='')
+                break
+
+        # If no factor is found, it means the number is prime
+        if not factor_found:
+            print(f"{number}")
+            break
 
 def main(filename):
     try:
         with open(filename, 'r') as file:
             for line in file:
                 number = int(line)
-                factors = factorize_number(number)
-                factors_str = '*'.join(map(str, factors))
-                print(f"{number}={factors_str}")
+                factorize_number(number)
     except FileNotFoundError:
         print(f"Error: File '{filename}' not found.")
     except Exception as e:
