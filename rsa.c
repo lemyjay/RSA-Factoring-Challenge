afdbba5fdf10aec9b52032ec6bee845e690f06e7
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-24 15:27:36 +0000

    the main, rsa.c

---
+++ rsa.c
@@ -0,0 +1,14 @@
+#include "rsa.h"
+/**
+ * main - Entry point to the program
+ * 
+ * @args: the number of arguments entered to the program
+ * @argv: an array containing the names of the arguments passed
+ * 
+ * Return: 0 for success
+ */
+int main(int args, char argv[])
+{
+
+    return (0);
+}
