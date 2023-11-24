3e537813c1ad026afa2978c878e29acb833d0e66
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-24 16:28:17 +0000

    implementation of the main

--- rsa.c
+++ rsa.c
@@ -1,14 +1,33 @@
 #include "rsa.h"
+
 /**
  * main - Entry point to the program
  * 
- * @args: the number of arguments entered to the program
+ * @argc: the number of arguments entered to the program
  * @argv: an array containing the names of the arguments passed
  * 
  * Return: 0 for success
  */
-int main(int args, char argv[])
+int main(int argc, char *argv[])
 {
+    FILE *file;
+
+    if (argc != 2)
+    {
+        fprint(stderr, "Usage: %s <file>\n", argv[0]);
+        return (EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        perror("Error opening file");
+        return (EXIT_FAILURE);
+    }
+
+    factorizeNumber(file);
+
+    fclose(file);
 
     return (0);
 }
