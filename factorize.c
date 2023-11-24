e6bf794d36c98b73a78dadc8e235c605e3e7f66a
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-24 16:46:54 +0000

    some changes to the rsa.c file

--- factorize.c
+++ factorize.c
@@ -1,40 +1,6 @@
 #include "rsa.h"
 
 /**
- * factorizeAndPrint - Factorizes a number and prints the result
- * 
- * @num: the number to factorize 
- */
-void factorizeAndPrint(long num)
-{
-    long i, originalNum = num;
-
-    while (num % 2 == 0)
-    {
-        printf("2*");
-        num /= 2;
-    }
-
-    for (i = 3; i * i <= num; i += 2)
-    {
-        while (num % i == 0)
-        {
-            printf("%ld", i);
-            num /= i;
-
-            if (num > 1)
-                printf("*");
-        }
-    }
-
-    if (num > 2)
-        printf("%ld", num);
-    
-    if (originalNum == 1)
-        printf("1");
-}
-
-/**
  * factorizeNumber - Factorizes numbers from the file
  *                      This function reads numbers from the specified
  *                   file, performs factorization and prints the results.
@@ -47,8 +13,15 @@ void factorizeNumber(FILE *file)
 
     while (fscanf(file, "%ld", &number) == 1)
     {
-        printf("%ld=", number);
-        factorizeAndPrint(number);
-        printf("\n");
+        /* Find factors of the number */
+        for (long factor = 2; factor <= number / 2; ++factor)
+        {
+            if (number % factor == 0)
+            {
+                /* Print the factorization */
+                printf("%ld=%ld*%ld\n", number, factor, number / factor);
+                break;
+            }
+        }
     }
 }
