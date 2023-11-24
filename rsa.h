2b34d8c2bd73e4dea63bd3d4f8230cd1ca1cc3f5
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-24 15:27:01 +0000

    The header file

---
+++ rsa.h
@@ -0,0 +1,10 @@
+#ifndef RSA_H
+#define RSA_H
+
+/*Libraries included to be included*/
+#include <stdlib.h>
+#include <stdio.h>
+
+/*Prototypes that would be used for this program*/
+
+#endif /*RSA_H*/
