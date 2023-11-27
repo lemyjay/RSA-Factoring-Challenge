68ac5edf99c58ba6eb236d1d14a2543e2d9065a0
ALENYORIGE Obed Alemya <lemyjay17@gmail.com>
2023-11-27 23:56:09 +0000

    changes

--- rsa.h
+++ rsa.h
@@ -4,10 +4,9 @@
 /*Libraries included to be included*/
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 /*Prototypes that would be used for this program*/
-long long gcd(long long a, long long b);
-long long pollards_rho(long long n);
-void factorize_number(long long number);
+void factorize(const char *filename);
 
 #endif /*RSA_H*/
